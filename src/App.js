ae516285dac2655678f70c549300c827c713a458
Paul Vangelakos <paulvangelakos@gmail.com>
2019-01-04 16:28:23 -0700

    More setup

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-    
+        <p>
+          <h1></h1>
+        sdfsdf
+        fd    qasasd
+        </p>
       </div>
     );
   }
