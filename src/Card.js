5f80bfb43b1d20b3aed41eae47a5a3e1cefaa726
Paul Vangelakos <paulvangelakos@gmail.com>
2019-01-05 17:36:18 -0700

    Matching up the user selected answer and the correct answer

--- src/Card.js
+++ src/Card.js
@@ -6,41 +6,50 @@ export default class Card extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedAnswer: '',
       answeredCorrect: false
     };
   }
 
-  selectAnswer = (event) => {
-    let answer = event.target.value;
-    this.checkAnswer();
-    this.setState({
-      selectedAnswer: answer
-    });
-  };
+  checkForAnswer = (event) => {
+    let userSelectedAnswer = event.target.value;
+    let correctAnswer = this.props.singleCommand.correctAnswer;
 
-  checkAnswer() {
-    if (this.state.selectedAnswer === this.props.singleCommand.correctAnswer) {
+    if (correctAnswer === userSelectedAnswer) {
       this.setState({
-        answeredCorrect: true 
-      })
+        answeredCorrect: true
+      });
     }
-  }
+  };
 
 
   render() {
-    return (
-      <div className="card-container">
-        <div className="question-container">
-          <h2>{this.props.singleCommand.question}</h2>
-          <h3>{this.props.singleCommand.gitCommand}</h3>
-        </div>
-        {
-          this.props.singleCommand.possibleAnswers.map((answer, index) => {
-            return <button className="possible-answer-buttons" key={index} value={answer} onClick={this.selectAnswer}>{answer}</button>;
+    if (this.state.answeredCorrect === false) {
+      return (
+        <div className="card-container">
+          <div className="question-container">
+            <h2>{this.props.singleCommand.question}</h2>
+            <h3>{this.props.singleCommand.gitCommand}</h3>
+          </div>
+          {
+            this.props.singleCommand.possibleAnswers.map((answer, index) => {
+              return (
+                <li>
+                  <input key={index} type="checkbox" label="form" value={answer} onClick={this.checkForAnswer}/>
 
-          })}
-      </div>
-    );
+                  {answer}
+
+                </li>
+              );      
+            })
+          }
+        </div>
+      );
+    } else {
+      return (
+        <div>
+              CORRECT!!!
+        </div>
+      );
+    }
   }
 }
